140de74868b9f4ef85a1790d8b6b3047547e77dc
Sajan <sajanmhatre29@gmail.com>
2021-10-20 01:54:57 +0530

    Use Function to Calculate Emp Wage

--- empWage.js
+++ empWage.js
@@ -3,21 +3,19 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const Wage_PER_HOUR = 20;
-
+//Calculate Employee Wage using Function
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;   
+        default:
+            return 0;
+    }    
+}
 let empHrs = 0;
 empCheck = Math.floor(Math.random()*10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;        
-        break;
-
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;        
-        break;
-
-    default:
-        empHrs = 0;
-        break;
-}
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * Wage_PER_HOUR;
-console.log("Emp Wage: "+ empWage);
+console.log("Emp Wage: " + empWage);
