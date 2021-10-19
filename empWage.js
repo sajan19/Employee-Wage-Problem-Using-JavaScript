e260600f1beff337e82c59608f2f1e110476ee66
Sajan <sajanmhatre29@gmail.com>
2021-10-19 20:38:34 +0530

    Check Employee Presence

--- empWage.js
+++ empWage.js
@@ -1,1 +1,10 @@
-console.log("Welcome to Employee Wage Problem...")
+console.log("Welcome to Employee Wage Problem...");
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}
+else {
+    console.log("Employee is Present")
+}
